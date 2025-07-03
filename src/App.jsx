7dfe5038246785cdad67af4600817dfcf0e0151a
Space Cadet 💻 <154667827+backendfrontflip@users.noreply.github.com>
2025-07-03 11:05:22 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ColorModeContext, useMode } from './theme'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+
+function App() {
+  const [theme, colorMode] = useMode();
+
+  return ( 
+    <div className="app"></div>
+  )
+}
+
+export default App
